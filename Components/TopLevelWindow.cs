6586a0f06c25e459060a232cdcd3846b883dfd7b
Marvin Herbold <marvin.herbold@gmail.com>
2025-11-10 10:23:33 -0700

    Fix to make TopLevelWindow not show up in the task switcher.

--- Components/TopLevelWindow.cs
+++ Components/TopLevelWindow.cs
@@ -1,12 +1,13 @@
 ﻿
-using System.Windows;
 using System.Windows.Interop;
+
+using static PInvoke.User32;
 
 namespace MarvinsAIRARefactored.Components;
 
 public class TopLevelWindow
 {
-	private Window? _window = null;
+	private HwndSource? _source;
 	public IntPtr WindowHandle { get; private set; } = 0;
 
 	public void Initialize()
@@ -15,24 +16,39 @@ public class TopLevelWindow
 
 		app.Logger.WriteLine( "[TopLevelWindow] Initialize >>>" );
 
-		_window = new Window
+		var hwndSourceParameters = new HwndSourceParameters( "MAIRA DirectInput Host" )
 		{
-			Width = 0,
-			Height = 0,
-			WindowStyle = WindowStyle.None,
-			ResizeMode = ResizeMode.NoResize,
-			ShowInTaskbar = false,
-			ShowActivated = false,
-			AllowsTransparency = true,
-			Opacity = 0
+			Width = 1,
+			Height = 1,
+			PositionX = -10000,
+			PositionY = -10000,
+			WindowStyle = unchecked((int) (
+				(uint) WindowStyles.WS_POPUP |
+				(uint) WindowStyles.WS_CLIPSIBLINGS |
+				(uint) WindowStyles.WS_CLIPCHILDREN
+			)),
+			ExtendedWindowStyle = unchecked((int) (
+				(uint) WindowStylesEx.WS_EX_TOOLWINDOW |	// hides from Alt+Tab
+				(uint) WindowStylesEx.WS_EX_NOACTIVATE		// don’t steal focus
+															// intentionally NOT WS_EX_APPWINDOW
+			))
 		};
 
-		_window.Show();
+		_source = new HwndSource( hwndSourceParameters );
 
-		var windowInteropHelper = new WindowInteropHelper( _window );
+		WindowHandle = _source.Handle;
 
-		WindowHandle = windowInteropHelper.Handle;
+		// Make sure it stays hidden (not strictly required; it has no taskbar/Alt+Tab presence anyway)
+		ShowWindow( WindowHandle, WindowShowStyle.SW_HIDE );
 
 		app.Logger.WriteLine( "[TopLevelWindow] <<< Initialize" );
+	}
+
+	public void Dispose()
+	{
+		_source?.Dispose();
+		_source = null;
+
+		WindowHandle = IntPtr.Zero;
 	}
 }
